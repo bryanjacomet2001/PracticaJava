9101899ebe5b47b36b78af6d5d3a5e612cb30e50
bryan <bj2257239@gmail.com>
2022-02-06 16:28:41 -0500

    Agregamos el metodo sumar

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+    }
+
+    public int Sumar(int n1, int n2){
+        int res = 0;
+        
+        res = n1 + n2;
+        
+        return res;
+    }
+}
