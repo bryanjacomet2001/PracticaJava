000d2d0b58471098a76a1f3902b24680b7ae69e4
bryan <bj2257239@gmail.com>
2022-02-06 16:42:52 -0500

    Se agregan las variables a ingresar y se llama al metodo sumar

--- src/App.java
+++ src/App.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-       
-    }
+        
+        int n1 = 0, n2 = 0, suma = 0, resta = 0, mult = 0;
+        App p = new App();
+        Scanner sc = new Scanner(System.in);
 
+
+        System.out.println("Ingrese el primer numero: ");
+        n1 = sc.nextInt();
+
+        System.out.println("Ingrese el primer segundo numero: ");
+        n2 = sc.nextInt();
+
+        suma = p.Sumar(n1, n2);        
+        System.out.println("El total de la suma es: "+suma);
+
+    }
+    
     public int Sumar(int n1, int n2){
         int res = 0;
         
@@ -10,4 +26,5 @@ public class App {
         
         return res;
     }
+
 }
